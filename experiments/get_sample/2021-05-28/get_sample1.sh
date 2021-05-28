cacce255a230f5cb39176e7e8dcca3d3b2e89b5a
Michael Laurila <michael.laurila@gmail.com>
2021-05-29 00:01:20 +0200

    Change location of output sample file

--- experiments/get_sample/2021-05-28/get_sample1.sh
+++ experiments/get_sample/2021-05-28/get_sample1.sh
@@ -1,2 +1,2 @@
 # Some arbritary chunk of a million consecutive rows.
-cat ../../../data/contigs.dat | tail -4000000 | head -1000000 > ../../../data/sample1.dat
+cat ../../../data/contigs.dat | tail -4000000 | head -1000000 > sample1.dat
