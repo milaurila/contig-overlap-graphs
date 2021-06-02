edb95aae4d8f2ab8f79e36f9f900468363797f67
Michael Laurila <michael.laurila@gmail.com>
2021-06-02 14:26:38 +0200

    Fix bug in reorder_edges.sh

--- experiments/prep_data/2021-06-01/reorder_edges.sh
+++ experiments/prep_data/2021-06-01/reorder_edges.sh
@@ -1,1 +1,1 @@
-awk '{if ($1<$2) print $2" "$1; else print $1" "$2}' < ../../../data/sample_translated_edges.dat | sort -n -k 1 > ../../../data/sample_translated_edges_reordered.dat
+awk '{if ($1<$2) print $1" "$2; else print $2" "$1}' < ../../../data/sample_translated_edges.dat | sort -n -k 1 > ../../../data/sample_translated_edges_reordered.dat
