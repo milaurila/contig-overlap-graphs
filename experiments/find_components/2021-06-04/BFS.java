69b98cb1e1649e2bdb8928bae9beb26fe9575eb7
Michael Laurila <michael.laurila@gmail.com>
2021-06-04 15:18:03 +0200

    Add BFS algo for finding components

---
+++ experiments/find_components/2021-06-04/BFS.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BFS {
+    ArrayList<ArrayList<Graph.Vertex>> components;
+
+    public BFS(Graph graph) {
+        Queue<Graph.Vertex> queue = new LinkedList<>();
+        components = new ArrayList<>();
+
+        for (Graph.Vertex vertex : graph.graphArray) {
+            if (!vertex.visited) {
+                ArrayList<Graph.Vertex> component = new ArrayList<>();
+                queue.add(vertex);
+                vertex.visited = true;
+
+                while (!queue.isEmpty()) {
+                    Graph.Vertex root = queue.remove();
+                    component.add(root);
+                    for (Graph.Vertex neighbour : root.neighbours) {
+                        if (!neighbour.visited) {
+                            queue.add(neighbour);
+                            neighbour.visited = true;
+                        }
+                    }
+                }
+                components.add(component);
+            }
+        }
+    }
+}
