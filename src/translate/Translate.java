342b2ba59a31fb20ffc401f33af7f495e50c89ae
Michael Laurila <michael.laurila@gmail.com>
2021-06-06 18:36:17 +0200

    Add output stream to doc comment of Translate.java

--- src/translate/Translate.java
+++ src/translate/Translate.java
@@ -5,6 +5,8 @@ public class Translate {
 
     /*
     Input stream: awk '{ print $1,$2 }' < contigs.dat
+
+    Output stream: id1 id2
      */
     public static void main(String[] args) {
         int intID = 0;
