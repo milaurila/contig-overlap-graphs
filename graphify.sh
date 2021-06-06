1dfe5eec14c88989dffa4f4145b42643517d4801
Michael Laurila <michael.laurila@gmail.com>
2021-06-06 03:15:18 +0200

    Add 'done' message

--- graphify.sh
+++ graphify.sh
@@ -2,4 +2,5 @@ numOfNodes=11393435;
 awk '{ print $1,$2 }' < data/contigs.dat | java -jar program/translate.jar |
     awk '{ if ($1<$2) print $1" "$2; else print $2" "$1 }' | sort | uniq |
     java -jar program/graphify.jar $numOfNodes > results/results.dat;
+echo Results saved in results/results.dat
 python program/plot.py < results/results.dat
