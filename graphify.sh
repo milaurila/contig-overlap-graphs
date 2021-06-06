e2c3132dbf3f5b3041c1235c6b75c740480fb39b
Michael Laurila <michael.laurila@gmail.com>
2021-06-06 02:57:59 +0200

    Add driver script

---
+++ graphify.sh
@@ -0,0 +1,5 @@
+numOfNodes=11393435;
+awk '{ print $1,$2 }' < data/contigs.dat | java -jar program/translate.jar |
+    awk '{ if ($1<$2) print $1" "$2; else print $2" "$1 }' | sort | uniq |
+    java -jar program/graphify.jar $numOfNodes > results/results.dat;
+python program/plot.py < results/results.dat
